eaf00338b8171e09ccada8970343c8b68b8def77
Delni <niko-d@live.fr>
2017-03-08 20:28:40 +0100

    Uncommitted work (?)

--- templates/footTemplate.php
+++ templates/footTemplate.php
@@ -4,6 +4,7 @@
       });
       </script>
     </div>
+    <script type="text/javascript" src="assets/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="assets/bootstrap-3.3.7/js/bootstrap.js"></script>
   </body>
 </html>
