71add370295febf0559ec11224a0fbdb2f29978b
Delni <niko-d@live.fr>
2017-03-20 22:34:43 +0100

    Dealing with all the git messed up thing

--- controller/AnonymousController.class.php
+++ controller/AnonymousController.class.php
@@ -117,31 +117,8 @@
     }
 
     public function validateRecovery($args){
-      $email=$args->read('recoPsw');
       if(User::eMailexist($email)){
         //TODO: send a e-mail
-        $mdp = $this->mdpGenerator();
-        $destinataire = $email;
-        // Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
-        $expediteur = 'adrien.handjani@yahoo.fr';
-        $objet = "Changement de MdP - 6 qui ramasse !";
-        $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
-        $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
-        $headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
-        $headers .= 'From: "Nom_de_expediteur"<'.$expediteur.'>'."\n"; // Expediteur
-        $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
-        $message = '<div style="width: 100%; text-align: center; font-weight: bold">Un Bonjour de Developpez.com !</div>';        $succes = mail($destinataire,$objet,$message,$headers);
-        if(mail($destinataire, $objet, $message, $headers)){
-          $view->setArg('conErrorText','mail envoyé!!!!!!');
-        }
-        else{
-          $view->setArg('conErrorText','fuuuuuuuuuu');
-        }
-        /*while($succes==FALSE){
-        $view = new ConnectionView($this);
-        $view->setArg('conErrorText','E-mail inconnu');
-        $view->render($this);
-      }*/
         $view = new ConnectionView($this);
         $view->setArg('RecoverText','Un e-mail avec vos identifiants a été envoyé !');
         $view->render($this);
