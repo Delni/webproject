8deaf1e4dbfbb894532cde3f41aaf7e899fb0e74
BLAZ\Adrien <adrien.handjani@minesdedouai.fr>
2017-03-22 11:37:15 +0100

    Joining Game function

--- model/Game.class.php
+++ model/Game.class.php
@@ -19,10 +19,22 @@
       $data= $res_sql->fetch(DatabasePDO::FETCH_OBJ);
       $res_sql2=DatabasePDO::getCurrentPDO()->query($sql2);
       $data2= $res_sql2->fetch(DatabasePDO::FETCH_OBJ);
-      if($data->nb_joueurs<10){
-        
-      } else {
-
+      if(!empty($data2)){
+        return true;
+      }
+      else{
+        if($data->estCommence=-1){
+          if($data->nb_joueurs<10){
+            $sql_req=DataBasePDO::getCurrentPDO()->prepare('INSERT INTO `jouer`(`ID_PLAT`,`PSEUDO`,`SCORE`) VALUES (:plat,:joueur,0)');
+            $sql_req->bindParam(':plat',$id_plat);
+            $sql_req->bindParam(':joueur',$login);
+            $sql_req->execute();
+            return(true);
+          }
+        }
+      else {
+        return(false);
+        }
       }
     }
   }
