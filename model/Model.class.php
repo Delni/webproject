6860ae7b3c3fad8d73bc189a1bf0d47045fe5690
Delni <niko-d@live.fr>
2017-03-22 17:06:11 +0100

    User now accept MVC (User.sql.php)

--- model/Model.class.php
+++ model/Model.class.php
@@ -8,6 +8,12 @@
       static::$sql_querries[$key]=$value;
     }
 
+    public static function exec_sql($sql_querry,$params){
+      $sql_req=DatabasePDO::getCurrentPDO()->prepare(static::$sql_querries[$sql_querry]);
+      $sql_req->execute($params);
+      return $sql_req->fetch(DatabasePDO::FETCH_OBJ);
+    }
+
 
   }
  ?>
