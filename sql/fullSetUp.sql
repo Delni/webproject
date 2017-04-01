588aec402ff34a00177bfed56fbe1becf10a626e
Delni <niko-d@live.fr>
2017-04-01 20:05:26 +0200

    Just some debug corrections

--- sql/fullSetUp.sql
+++ sql/fullSetUp.sql
@@ -121,6 +121,16 @@ CREATE TABLE Score(
 
 ALTER TABLE Pile ADD CONSTRAINT Fk_Id_Plat FOREIGN KEY (Id_Plat) REFERENCES Plateau(Id_Plat);
 
+
+--  ad88888ba        db        88b           d88 88888888ba  88          88888888888 ad88888ba
+-- d8"     "8b      d88b       888b         d888 88      "8b 88          88         d8"     "8b
+-- Y8,             d8'`8b      88`8b       d8'88 88      ,8P 88          88         Y8,
+-- `Y8aaaaa,      d8'  `8b     88 `8b     d8' 88 88aaaaaa8P' 88          88aaaaa    `Y8aaaaa,
+--   `"""""8b,   d8YaaaaY8b    88  `8b   d8'  88 88""""""'   88          88"""""      `"""""8b,
+--         `8b  d8""""""""8b   88   `8b d8'   88 88          88          88                 `8b
+-- Y8a     a8P d8'        `8b  88    `888'    88 88          88          88         Y8a     a8P
+--  "Y88888P" d8'          `8b 88     `8'     88 88          88888888888 88888888888 "Y88888P"
+
 USE `6nimmt`;
 
 INSERT INTO Joueur VALUES ('SeigneurSith', 'JeSuisTonPère', 'Vador', 'Dark', NULL, NULL, NULL, NULL,0,0);
